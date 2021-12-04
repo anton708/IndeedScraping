0d45f6df1f3d22ed721645585ee26426ce1924ab
anton708 <59750039+anton708@users.noreply.github.com>
2021-12-03 19:00:02 -0500

    Add files via upload

--- indeedFlask.py
+++ indeedFlask.py
@@ -7,11 +7,40 @@ Created on Fri Dec  3 16:57:12 2021
 """
 
 from flask import Flask
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-def helloworld():
-    return "Hello World"
+@app.route('/<company>',methods=['GET'])
+def getReviews(company):
+    reviews = []
+    pages = [20,40,60,80,100,120,140,160,180,200]
+    for page in pages:
+        base_url= 'https://www.indeed.com/cmp/{}/reviews?start=' + str(page)
+        url = base_url.format(company)
+        
+        header = {"User-Agent":"Mozilla/5.0 Gecko/20100101 Firefox/33.0 GoogleChrome/10.0"}
+        page = requests.get(url,headers = header)
+        
+        
+        soup = BeautifulSoup(page.content, 'lxml')
+        
+        revs = soup.find_all("span", class_="css-1cxc9zk e1wnkr790")
+        
+        for rev in revs:
+            if "aria" in str(rev):
+                text = str(rev).replace('''<span class="css-82l4gy eu4oa1w0">''',"").replace("</span>","").replace('''<br aria-hidden="true"/>''',"").replace('''<span aria-hidden="false" class="css-1cxc9zk e1wnkr790" lang="en-AU">''',"").replace("<span>","").replace('''<span aria-hidden="false" class="css-1cxc9zk e1wnkr790">''',"")
+    
+                if text!=" " and text!="" and text is not None:
+                    reviews.append(text)
+
+
+    reviews = list(set(reviews))
+    return reviews
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
